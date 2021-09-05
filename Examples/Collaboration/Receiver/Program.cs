efe3a6994151edc98d5db8d8ba0b51ca51c29891
clown <tt.clown@gmail.com>
2021-09-05 23:55:11 +0900

    Add Collaboration project.

---
+++ Examples/Collaboration/Receiver/Program.cs
@@ -0,0 +1,95 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+using System.IO;
+using Cube.Collections;
+using Cube.FileSystem.DataContract;
+using Cube.Logging;
+using Cube.Pdf.Converter;
+
+namespace Collaboration.Receiver
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Program
+    ///
+    /// <summary>
+    /// CubeVP 連携デモ (CoReceiver) のメインプログラムを表します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    class Program
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Main
+        ///
+        /// <summary>
+        /// CoReceiver のエントリポイントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        static void Main(string[] args)
+        {
+            // ログ出力用の処理です。不要な場合、削除して構いません。
+            InitLog(args);
+
+            // まず、プログラム引数を ArgumentCollection クラスを用いて解析します。
+            // 今回の連携デモでは JSON データの保存されているパスが DocumentName
+            // オプション引数に指定されているため、SettingFolder クラスに対して
+            // Format.Json および DocumentName の値を指定して初期化します。
+            // その後、Load メソッドを実行する事により JSON 形式の設定内容が読み込まれます。
+            var src = new ArgumentCollection(args);
+            var settings = new SettingFolder(Format.Json, src.Options["DocumentName"]);
+            settings.Load();
+
+            // SettingFolder オブジェクトに対して、プログラム引数の内容を反映させるため
+            // Set メソッドを実行します。
+            // ただし、Set メソッドは Destination （保存場所）の値を上書きします。
+            // 今回の連携デモでは、JSON データに記載された場所に PDF ファイルを保存する事を
+            // 想定しているため、ローカル変数にいったん退避させた後、Set メソッド適用後に
+            // 再度その値を反映させる事とします。
+            var dest = settings.Value.Destination;
+            settings.Set(src);
+            settings.Value.Destination = dest;
+
+            // 設定が完了したら、Facade クラスで変換処理を実行します。
+            using (var facade = new Facade(settings)) facade.Invoke();
+
+            // 最後に、印刷前プログラムが作成した一時ファイルを削除します。
+            File.Delete(settings.Location);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// InitLog
+        ///
+        /// <summary>
+        /// ログ出力に関する初期設定を実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private static void InitLog(string[] args)
+        {
+            var src = typeof(Program);
+            _ = Logger.ObserveTaskException();
+            src.LogInfo(src.Assembly);
+            src.LogInfo($"[ {string.Join(" ", args)} ]");
+        }
+    }
+}
---
+++ Examples/Collaboration/Sender/Program.cs
@@ -0,0 +1,121 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+using Cube.FileSystem.DataContract;
+using Cube.Logging;
+using Cube.Pdf.Converter;
+
+namespace Collaboration.Sender
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Program
+    ///
+    /// <summary>
+    /// CubeVP 連携デモ (CoSender) のメインプログラムを表します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    class Program
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Main
+        ///
+        /// <summary>
+        /// CoSender のエントリポイントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        static void Main(string[] args)
+        {
+            // ログ出力用の処理です。不要な場合、削除して構いません。
+            InitLog(args);
+
+            // まず、JSON データを保存するための一時ファイル名を決定します。
+            // 次に、SettingFolder クラスに対して Format.Json および
+            // 先ほど決定したパスを指定して初期化します。
+            var json = Path.GetTempFileName();
+            var settings = new SettingFolder(Format.Json, json);
+
+            // 初期化完了後、Value プロパティに対して必要な設定内容を反映させます。
+            // 例として、マイ・ドキュメントに cubevp-collaboration.pdf と言う
+            // ファイル名で保存し、保存されたフォルダーを開くように設定してみます。
+            // 必要な設定が完了したら、Save メソッドで設定内容を JSON 形式で保存します。
+            var dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            settings.Value.Destination = Path.Combine(dir, "cubevp-collaboration.pdf");
+            settings.Value.PostProcess = PostProcess.OpenDirectory;
+            settings.Save();
+
+            // PrintDocument クラスの DocumentName プロパティに対して
+            // 最初に決定した一時ファイルのパスを指定します。
+            // その後、印刷ダイアログの表示等、必要な印刷処理を実行して終了です。
+            var doc = new PrintDocument { DocumentName = json };
+            var dialog = new PrintDialog { Document = doc };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                doc.PrintPage += PrintDummy;
+                doc.Print();
+            }
+            else File.Delete(json);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PrintDummy
+        ///
+        /// <summary>
+        /// ダミーデータの印刷処理を実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private static void PrintDummy(object s, PrintPageEventArgs e)
+        {
+            var text  = "CubeVP collaboration demo.";
+            var font  = SystemFonts.DefaultFont;
+            var brush = SystemBrushes.WindowText;
+            var x = e.PageSettings.PaperSize.Width  * 0.1f;
+            var y = e.PageSettings.PaperSize.Height * 0.1f;
+
+            e.Graphics.DrawString(text, font, brush, x, y);
+            e.HasMorePages = false;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// InitLog
+        ///
+        /// <summary>
+        /// ログ出力に関する初期設定を実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private static void InitLog(string[] args)
+        {
+            var src = typeof(Program);
+            _ = Logger.ObserveTaskException();
+            src.LogInfo(src.Assembly);
+            src.LogInfo($"[ {string.Join(" ", args)} ]");
+        }
+    }
+}
